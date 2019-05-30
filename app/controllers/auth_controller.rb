f971efdd34ca85b7e2ed1f42d69d016659da59b6
Paul Ardeleanu <paul.ardeleanu@vonage.com>
2019-05-30 13:41:17 +0100

    improved login

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -11,16 +11,21 @@ class AuthController < ApplicationController
   end
 
 
-  # On login, the api key and secret are stored in session variables
+  # On login, the api key and secret are checked and stored in session variables
   def login_do
     api_key = params[:api_key]
     api_secret = params[:api_secret]
-    if !api_key.blank? && !api_secret.blank?
-        session[:api_key] = api_key
-        session[:api_secret] = api_secret
-        redirect_to app_url, notice: "Logged in!"
+    if api_key.blank? || api_secret.blank?
+      redirect_to root_url, alert: "Api credentials are invalid..."
+      return
+    end
+    @existing_apps = NexmoApi.apps(api_key, api_secret)
+    if @existing_apps == nil
+      redirect_to root_url, alert: "Api credentials are invalid."
     else
-        redirect_to login_url, alert: "Api credentials are invalid"
+      session[:api_key] = api_key
+      session[:api_secret] = api_secret
+      redirect_to app_url, notice: "Logged in!"
     end
   end
 
